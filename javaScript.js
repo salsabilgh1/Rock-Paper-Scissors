53ac544ea110787e94ce846ecac981ff60a91dcf
salsabil <salsabilghennam1@gmail.com>
2025-08-14 16:26:37 +0100

    created the a function to get a rondom choice from computer

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,13 @@
 console.log("Hello World!")
+function getComputerChoice(){
+    let choice=Math.floor(Math.random()*3+1);
+    if(choice===1){
+        return "Rock";
+        
+    }else if(choice===2){
+        return "Paper"
+    }else{
+        return "Scissors"
+    }
+}
+console.log(getComputerChoice())
