eeb6deeddbcff0422c6601f09135dbc4c347c5bb
salsabil <salsabilghennam1@gmail.com>
2025-08-14 17:07:02 +0100

    created the function playRound

--- javaScript.js
+++ javaScript.js
@@ -19,3 +19,20 @@ function getHumenChoice(){
 }
 let humanScor=0;
 let computerScore=0;
+
+function playRound(){
+    let humanChoice=getComputerChoice().toLowerCase();
+    let computerChoice=getComputerChoice().toLowerCase();
+    if(humanChoice===computerChoice){
+        console.log("it's a draw!")
+    }
+    else{
+        if(humanChoice==="rock"&&computerChoice==="scissors"||humanChoice==="paper"&&computerChoice==="rock"||humanChoice==="scissors"&&computerChoice==="paper"){
+            console.log("you win! "+ humanChoice+" beats "+computerChoice)
+            humanScor++;
+        }else{
+            console.log("you lose! "+ computerChoice+" beats "+humanChoicerChoice)
+            computerScore++;
+        }
+    }
+}
