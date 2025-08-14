555438541e636dcb1e103edc1109f5bd85777ad9
salsabil <salsabilghennam1@gmail.com>
2025-08-14 18:36:28 +0100

    created the play game function and finished the programe

--- javaScript.js
+++ javaScript.js
@@ -21,7 +21,7 @@ let humanScor=0;
 let computerScore=0;
 
 function playRound(){
-    let humanChoice=getComputerChoice().toLowerCase();
+    let humanChoice=getHumenChoice().toLowerCase();
     let computerChoice=getComputerChoice().toLowerCase();
     if(humanChoice===computerChoice){
         console.log("it's a draw!")
@@ -31,8 +31,23 @@ function playRound(){
             console.log("you win! "+ humanChoice+" beats "+computerChoice)
             humanScor++;
         }else{
-            console.log("you lose! "+ computerChoice+" beats "+humanChoicerChoice)
+            console.log("you lose! "+ computerChoice+" beats "+humanChoice)
             computerScore++;
         }
     }
 }
+function palyGame(){
+    for(i=0; i<5 ;i++){
+        console.log("round "+ i +" start")
+        playRound();
+    }
+    console.log("scors:\n")
+    console.log("You: "+ humanScor)
+    console.log("computer: "+computerScore)
+    if(humanScor>computerScore){
+        console.log("you won the game!")
+    }else{
+        console.log("you lose")
+    }
+}
+palyGame()
